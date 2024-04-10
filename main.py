d143eff89aaa623dfb8c9f09b7b4009fdbb97692
carldamey <carldamey@gmail.com>
2024-04-10 16:47:51 -0700

    refactors new file protocol

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ if os.path.exists("data.csv"):
 else:
   if input("No Data Found, Create Data File? [Y/N]: ").upper() == "Y":
     data = {    
-      "comp_points": [0],
-      "comp_progress": [0],
+      "comp_points": [int(prompt("How many competitive points do you have? "))],
+      "comp_progress": [int(prompt("What is your current competitive progress? "))],
     }
     dataframe = pandas.DataFrame(data)
     dataframe.to_csv("data.csv", index=False)
