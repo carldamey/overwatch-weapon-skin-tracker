3918cfdb1d8e6da45049e5c9a00e05e543a7c752
carldamey <carldamey@gmail.com>
2024-04-10 16:21:27 -0700

    creates file detection, csv integration, and functions for new data and saving

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-# if no data file, create one
+import os, csv, pandas
+
+# DETECT SAVE DATA
+if os.path.exists("data.csv"):
+  print("Data Found!")
+else:
+  if input("No Data Found, Create Data File? [Y/N]: ").upper() == "Y":
+      new_data()
+      save()
+  else:
+    print("Okay bye.") 
+    input("")
+    exit
 
 # read data
+data = pandas.read_csv("data.csv")
+print(data)
 
 # if no data 
 
@@ -13,3 +27,15 @@
 # each game is % with n winrate (default 50)
 
 # you need n more wins, each is %
+
+# delete data option
+
+def new_data():
+  data = {
+    "comp_progress": [0],
+    "comp_points": [0],
+  }
+
+def save():
+  dataframe = pandas.DataFrame(data)
+  dataframe.to_csv("data.csv", index=False)
