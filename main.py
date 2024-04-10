51818c94d3dc43adc0c561a258c4d26015ee9b6d
carldamey <carldamey@gmail.com>
2024-04-10 15:43:10 -0700

    creates pseudocode

---
+++ main.py
@@ -0,0 +1,15 @@
+# if no data file, create one
+
+# read data
+
+# if no data 
+
+# add one game , update progress, update points
+
+# you are % closer 
+
+# you are % to jade
+
+# each game is % with n winrate (default 50)
+
+# you need n more wins, each is %
