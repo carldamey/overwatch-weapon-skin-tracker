fb2fa24220966adb56dfc3e08f867faa94df6648
carldamey <carldamey@gmail.com>
2024-04-12 17:17:29 -0700

    removes commented out code, creates manual update feature

--- main.py
+++ main.py
@@ -1,56 +1,3 @@
-# import os, csv, pandas
-
-# # def new_data():
-# #   data = {    
-# #     "comp_points": [0],
-# #     "comp_progress": [0],
-# #   }
-
-# def save():
-#   dataframe = pandas.DataFrame(data)
-#   dataframe.to_csv("data.csv", index=False)
-
-# # DETECT SAVE DATA
-# if os.path.exists("data.csv"):
-#   print("Data Found!")
-# else:
-#   if input("No Data Found, Create Data File? [Y/N]: ").upper() == "Y":
-#     data = {    
-#       "comp_points": [int(input("How many competitive points do you have? "))],
-#       "comp_progress": [int(input("What is your current competitive progress? "))],
-#     }
-#     dataframe = pandas.DataFrame(data)
-#     dataframe.to_csv("data.csv", index=False)
-#   else:
-#     print("Okay bye.") 
-#     input("")
-#     exit
-
-# def save():
-#   dataframe = pandas.DataFrame(data)
-#   os.remove("data.csv")
-#   dataframe.to_csv("data.csv", index=False)
-
-# # read data
-# data = pandas.read_csv("data.csv", names=["comp_points", "comp_progress"])
-# print(data["comp_points"][1])
-
-
-# if choice == "A":
-#   choice = input("Win [W] or Loss [L]? ").upper()
-#   if choice == "W":
-#     data["comp_points"][1] = int(data["comp_points"][1]) + 10
-#     data["comp_progress"][1] = int(data["comp_points"][1]) + 3
-#   elif choice == "L":
-#     data["comp_progress"][1] = int(data["comp_progress"][1]) + 1
-
-#   if int(data["comp_progress"][1]) >= 30:
-#     data["comp_progress"][1] -= 30
-#     data["comp_points"][1] += 100
-#   save()
-#   choice = ""
-
-
 # you are % closer
 
 # you are % to jade
@@ -63,9 +10,10 @@
 
 import csv, math, os, pandas
 
-# comp_points = 0
-# comp_progress = 0
 data = {}
+running = True
+
+# TODO see if data = {} can be removed by making a fallback in the save function
 
 def save():
   dataframe = pandas.DataFrame({
@@ -94,7 +42,7 @@ elif not os.path.exists("data.csv"):
 data = load()
 print("Data loaded.", print(data))
 
-while True:
+while running:
   load()
   print(f"You have {data['comp_points']} Points and {data['comp_progress']} / 30 Progress.")
   primary_choice = input("What would you like to do?\n[A] Add Game\n[V] View Stats\n[U] Update Stats Manually\n[X] Exit Program\n").upper()
@@ -103,7 +51,7 @@ while True:
     # ADD GAME RESULT
     case "A":
       print("Add Game")
-      win_loss_choice = input("Win [W] Loss [L] or Cancel [X]?\n").upper()
+      win_loss_choice = input("[W] Win\n[L] Loss\n[X] Cancel\n").upper()
       match win_loss_choice:
 
         # GAME WON
@@ -119,7 +67,7 @@ while True:
 
         # CANCELLED
         case "X":
-          print("Canceled")
+          print("Canceled.")
 
         # INVALID WIN/LOSS SELECTION
         case _,:
@@ -133,16 +81,25 @@ while True:
       print("View Stats")
 
       print(f"With a 50% win rate, you need to play {math.ceil((3000 - data['comp_points']) / 11.665)} more games.")
-      #TODO MAKE THIS MORE ACCURATE BY CALCULATING EXACT PROGRESS
+      # TODO MAKE THIS MORE ACCURATE BY CALCULATING EXACT PROGRESS
 
     case "U":
       print("Manually Update Stats")
-      manual_update_choice = ""
-      
+      update_choice = input("[P] Update Competitive Point Count\n[G] Update Competitive Progress Count\n[X] Cancel").upper()
+      match update_choice:
+        case "P":
+          data["comp_points"] = int(input("How many Competitive Points do you have?\n"))
+        case "G":
+          data["comp_progress"] = int(input("How much Competitive Progress / 30 do you have?"))
+        case "X":
+          print("Cancelled.")
+        case _:
+          print("Invalid selection.")
 
+      
     case "X":
       print("Exit Program")
-      break
+      running = False
 
     case _:
       print("Invalid selection.")
