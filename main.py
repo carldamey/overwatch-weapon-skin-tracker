f9028a59adefb2d397e79957b6bafe3f03cbb121
carldamey <carldamey@gmail.com>
2024-04-16 11:21:33 -0700

    begins implementation of error handling and makes the data input DRYer

--- main.py
+++ main.py
@@ -3,19 +3,31 @@ data = {}
 running = True
 
 def save():
+  print("DATAafjldshfkjsldfhkjsfh", data)
   dataframe = pandas.DataFrame({
     "comp_points": [data["comp_points"]],
     "comp_progress": [data["comp_progress"]],
   })
   dataframe.to_csv("data.csv", index=False)
 
+def new_data():
+  input_data = {
+    "comp_points": "",
+    "comp_progress": "",
+  }
+  while not input_data["comp_points"].isdigit():
+    input_data["comp_points"] = input("How many competitive points do you have?\n")
+  while not input_data["comp_progress"].isdigit():
+    input_data["comp_progress"] = input("What is your current competitive progress / 30?\n")
+  return input_data
+
 def load():
   if os.path.exists("data.csv"):
     print("Data Found")
   elif not os.path.exists("data.csv"):
     print("No data found, creating data file.")
-    data["comp_points"] = input("How many competitive points do you have?\n")
-    data["comp_progress"] = input("What is your current competitive progress / 30?\n")
+    data = new_data()
+    print("DATAAJDKLSDJLASJD", data)
     save()
   csv_data = pandas.read_csv("data.csv", names=["comp_points", "comp_progress"])
   return {
@@ -83,19 +95,21 @@ while running:
         # CALCULATE REMAINING GAMES
         case "G":
           calculate_games_left(int(input("What % is your winrate?\n")))
+          # TODO ADD CHECKS FOR THIS TOO
 
     case "U":
       print("Manually Update Stats")
-      update_choice = input("[P] Update Competitive Point Count\n[G] Update Competitive Progress Count\n[X] Cancel\n").upper()
-      match update_choice:
-        case "P":
-          data["comp_points"] = int(input("How many Competitive Points do you have?\n"))
-        case "G":
-          data["comp_progress"] = int(input("How much Competitive Progress / 30 do you have?\n"))
-        case "X":
-          print("Canceled.")
-        case _:
-          print("Invalid selection.")
+      data = new_data()
+      # update_choice = input("[P] Update Competitive Point Count\n[G] Update Competitive Progress Count\n[X] Cancel\n").upper()
+      # match update_choice:
+      #   case "P":
+      #     data["comp_points"] = int(input("How many Competitive Points do you have?\n"))
+      #   case "G":
+      #     data["comp_progress"] = int(input("How much Competitive Progress / 30 do you have?\n"))
+      #   case "X":
+      #     print("Canceled.")
+      #   case _:
+      #     print("Invalid selection.")
 
     case "X":
       print("Exit Program")
@@ -105,9 +119,10 @@ while running:
       print("Invalid selection.")
 
   if data["comp_points"] >= 3000:
-    input("Congrats, you now have enough Competitive Points to Purchase your skin! Please make your purchase and then press [ENTER]")
+    input("Congrats, you now have enough Competitive Points to Purchase your weapon skin! Please make your purchase and then press [ENTER]")
     data["comp_points"] -= 3000
 
   save()
 
   # TODO create fallback if non-numbers are entered, or other invalid selections
+  # TODO ADD SCREEN CLEARING
